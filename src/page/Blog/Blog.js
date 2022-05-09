be86c39417447064dbc23f0400497b35ca16285f
RafiUlI <ctgrafi150@gmail.com>
2022-05-09 17:32:15 +0600

    fixed blog

--- src/page/Blog/Blog.js
+++ src/page/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Blog.css';
 const Blog = () => {
     return (
-        <div>
+        <div className='container'>
             <h1>Ques of some answer:</h1>
             <br />
             <br />
@@ -20,6 +20,7 @@ const Blog = () => {
                 3. Nodejs server side e use hoi.
             </p>
             <br />
+            <br />
 
 
 
@@ -28,13 +29,30 @@ const Blog = () => {
 
 
             <h2>2. Different between SQL and NOSQL:</h2>
+            <h3>SQL:</h3>
+            <p>
+                1.SQL HOLO RELATIONAL DATABASE MANAGEMENT SYSTEM .
+                2. sql database static hoye thake.
+                3. Sql vertically holo scalable.
+            </p>
+            <h3>NOSQL:</h3>
+            <p>
+                1. Nosql holo Non-relational ba distributed database.
+                2. Nosql database daynamic hoye thake.
+                3. Nosql horizentically holo scalable.
+            </p>
+            <br />
+            <br />
+
+
+
+            <h2>3. what is the purpose of jwt and how dose it work?:</h2>
             <p>
                 .Jwt holo popular sucuring api . ja client e token toiri kre server er data k onno person theke secure krar purpose e use hoi.
                 <br />
                 Jwt je token create kre ta onno user er token er sathe differance toiri kre and ekti set of claims dharon kre.claim gulo token issue kari ke sonakto kore.jot token e 3 ta part : 1.header, 2.payload, 3.signature.
-                Header token er type dharon kore. payload claim gulo contain kore.signeture token thik ache kina jacai kore
+                Header token er type dharon kore.payload claim gulo contain kore.signeture token thik ache kina jacai kore.
             </p>
-
         </div>
     );
 };
